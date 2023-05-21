3077a9cadd44004b4cd2920629cfc14febd0564e
hieubyui <crysector@gmail.com>
2023-05-20 20:36:21 -0700

    prep2

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("Type your first name: ");
+        string first = Console.ReadLine();
+
+        Console.WriteLine("Type your last name: ");
+        string last = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {last}, {first} {last}.");
     }
 }
